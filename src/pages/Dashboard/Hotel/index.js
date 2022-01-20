98dacd07125f33c156deb239673803a559c05203
liggia <liggia.cruz@gmail.com>
2022-01-20 12:40:09 -0300

    feat: hotel page permission

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,24 +1,43 @@
 import Typography from "@material-ui/core/Typography";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import styled from "styled-components";
+import UserContext from "../../../contexts/UserContext";
 import HotelOption from "./HotelOption";
 import Room from "./Room";
 
 export default function Hotel() {
   const [hotelSelected, setHotelSelected] = useState("");
   const [roomSelected, setRoomSelected] = useState("");
+  const [userStatus, setUserStatus] = useState({ allow: false, message: "" });
+  const { userData } = useContext(UserContext);
+
+  useState(() => {
+    if(userData.user.id === 4 && userData.user.ticket.id === 1) {
+      setUserStatus({ ...userStatus, allow: true });
+    }
+    if(userData.user.id <= 3) {
+      setUserStatus({ ...userStatus, message: "Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem" });
+    }
+    else {
+      setUserStatus({ ...userStatus, message: "Sua modalidade de ingresso não inclui hospedagem. Prossiga para a escolha de atividades" });
+    }
+  }, []);
 
   return(
     <Container>
       <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
-      <Subtitle>Primeiro, escolha seu hotel</Subtitle>
-      <OptionsContainer>
-        {hoteis.map((h) => <HotelOption setHotelSelected={setHotelSelected} hotelSelected={hotelSelected} id={h.id}/> )}
-      </OptionsContainer>
-      <Subtitle>Ótima pedida! Agora escolha seu quarto:</Subtitle>
-      <OptionsContainer>
-        {quartos.map((room) => <Room roomSelected={roomSelected} setRoomSelected={setRoomSelected} id={room.id}/>)}
-      </OptionsContainer>
+      {userStatus.allow ?
+        <>
+          <Subtitle>Primeiro, escolha seu hotel</Subtitle>
+          <OptionsContainer>
+            {hoteis.map((h) => <HotelOption key={h.id} setHotelSelected={setHotelSelected} hotelSelected={hotelSelected} id={h.id}/> )}
+          </OptionsContainer>
+          <Subtitle>Ótima pedida! Agora escolha seu quarto:</Subtitle>
+          <OptionsContainer>
+            {quartos.map((room) => <Room key={room.id} roomSelected={roomSelected} setRoomSelected={setRoomSelected} id={room.id}/>)}
+          </OptionsContainer>
+        </>
+        : <ErrorMessage>{userStatus.message}</ErrorMessage>}
     </Container>
   );
 }
@@ -50,6 +69,17 @@ const OptionsContainer = styled.div`
   height: auto;
   margin-bottom: 30px;
   flex-wrap: wrap;
+`;
+
+const ErrorMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  color: #8E8E8E;
+  font-size: 20px;
+  width: 55%;
+  margin: auto;
+
 `;
 
 const hoteis = [{ id: 1 }, { id: 2 }, { id: 3 }];
